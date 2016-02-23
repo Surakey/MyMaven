8a0abbc3e41fd35ceecb9784fdaf68fa3be159a7
Surakey <460013159@qq.com>
2016-02-23 11:18:31 +0800

    delect test

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,7 +2,7 @@
  * Created by yd_qa_03 on 2016/2/22.
  */
 public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("Hello World!");// test
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
